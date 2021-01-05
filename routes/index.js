654aac53742511b8e6d472e3223a8c452731a35b
Sylvain Durant <sy.durant@hotmail.com>
2021-01-05 16:16:36 +0100

    all pages up

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,53 @@ router.get('/', (req,res)=>{
     res.render('index');
 })
 
+//about page
+router.get('/about', (req,res)=>{
+    res.render('about');
+})
+
+//cart page
+router.get('/cart', (req,res)=>{
+    res.render('cart');
+})
+
+//checkout page
+router.get('/checkout', (req,res)=>{
+    res.render('checkout');
+})
+
+//contact-us page
+router.get('/contact-us', (req,res)=>{
+    res.render('contact-us');
+})
+
+//gallery page
+router.get('/gallery', (req,res)=>{
+    res.render('gallery');
+})
+
+//my-account page
+router.get('/my-account', (req,res)=>{
+    res.render('my-account');
+})
+
+//shop page
+router.get('/shop', (req,res)=>{
+    res.render('shop');
+})
+
+//shop-details page
+router.get('/shop-details', (req,res)=>{
+    res.render('shop-details');
+})
+
+//wishlist page
+router.get('/wishlist', (req,res)=>{
+    res.render('wishlist');
+})
+
+//////////////////////////////////////////////////////////////////////////////
+
 //register page
 router.get('/register', (req,res)=>{
     res.render('register');
